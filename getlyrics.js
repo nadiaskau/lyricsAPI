bb43839f9aadd56e5fe4831a16606830b9425d74
nadiaskau <nadiaskau@hotmail.com>
2021-04-06 12:06:20 +0200

    get lyrics

---
+++ getlyrics.js
@@ -0,0 +1,39 @@
+
+let lastFM_APIkey = "f59b1842961e265d09326395fb461985";
+let toptracksAPI = `http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=denmark&api_key=${lastFM_APIkey}&format=json`; 
+let toptracks; 
+
+//Getting our top tracks
+
+    fetch(toptracksAPI)
+    .then(response => response.json())
+    .then(function(data){
+        let dropdown = document.getElementById("toptracks");
+        dropdown.addEventListener('change', getLyrics);
+        for (let i = 0; i < data.tracks.track.length; i++) {
+            let option = document.createElement('option'); 
+            option.innerHTML = data.tracks.track[i].name + " - " + data.tracks.track[i].artist.name; 
+            option.setAttribute('id', i);
+            dropdown.appendChild(option);
+        }
+        toptracks = data.tracks.track;
+        return toptracks;
+        });
+
+
+function getLyrics(ev){
+    
+    let index = ev.target[ev.target.selectedIndex].id;
+    let artist = toptracks[index].artist.name; 
+    let title = toptracks[index].name;
+    let lyricsAPI = `https://api.lyrics.ovh/v1/${artist}/${title}`; 
+    let lyrics = document.getElementById("lyrics");
+
+    fetch(lyricsAPI)
+    .then(response => response.json())
+    .then(function(data){
+        lyrics.innerHTML = data.lyrics;
+    });
+}
+
+
